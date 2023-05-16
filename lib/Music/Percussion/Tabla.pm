86859ece653d7dd7e9e4bdd4f9cb60ba55e73580
Gene Boggs <gene@ology.net>
2023-05-16 14:04:50 -0700

    Bump version

--- lib/Music/Percussion/Tabla.pm
+++ lib/Music/Percussion/Tabla.pm
@@ -2,7 +2,7 @@ package Music::Percussion::Tabla;
 
 # ABSTRACT: Play the tabla!
 
-our $VERSION = '0.0101';
+our $VERSION = '0.0102';
 
 use lib map { "$ENV{HOME}/sandbox/$_/lib" } qw(MIDI-Util MIDI-Drummer-Tiny);
 
