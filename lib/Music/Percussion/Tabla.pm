5a8e4422a1bde55c11158861ee1b33723ba9de4b
Gene Boggs <gene@ology.net>
2025-01-25 09:52:40 -0800

    WIP

--- lib/Music/Percussion/Tabla.pm
+++ lib/Music/Percussion/Tabla.pm
@@ -130,7 +130,8 @@ sub BUILD {
   $tabla->dhun($duration);
   $tabla->dhun($duration, $index);
 
-Where the B<duration> is a note length like C<$tabla-E<gt>eighth>
+Where the B<duration> is a note length like C<$tabla-E<gt>eighth>.
+
 Defaults:
 
   dhun: 67 80
