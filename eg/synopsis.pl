fbbf78e91620f412f698b7e3a9c5aeb13c3af685
Gene Boggs <gene@ology.net>
2025-01-26 13:28:57 -0800

    Fresh

--- eg/synopsis.pl
+++ eg/synopsis.pl
@@ -11,15 +11,19 @@ my $t = Music::Percussion::Tabla->new;
 # print $t->soundfont, "\n";
 
 for (1 .. $t->bars) {
-  $t->strike('ta', $t->eighth);
-  $t->strike('ta', $t->eighth);
-  $t->strike('tun');
-  $t->strike('ge');
-  $t->rest($t->quarter);
+    $t->strike('ta', $t->eighth);
+    $t->strike('ta', $t->eighth);
+    $t->strike('tun');
+    $t->strike('ge');
+    $t->rest($t->quarter);
 }
 $t->double_strike('dha', $t->quarter);
 
 $t->teentaal($t->eighth) for 1 .. $t->bars;
+$t->keherawa($t->eighth) for 1 .. $t->bars;
+$t->jhaptaal($t->eighth) for 1 .. $t->bars;
+$t->dadra($t->eighth) for 1 .. $t->bars;
+$t->rupaktaal($t->eighth) for 1 .. $t->bars;
 
 $t->play_with_timidity;
 # OR:
